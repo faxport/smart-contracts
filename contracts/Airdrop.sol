f04dd75fe05ec8fb5080700a161a03948e381cf4
Dylan <dylanninin@gmail.com>
2018-04-03 14:28:47 +0800

    Add Airdrop

---
+++ contracts/Airdrop.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.18;
+
+
+import './Ownable.sol';
+import './TokenERC20Interface.sol';
+
+
+contract Airdrop is Ownable {
+
+  function multisend(address _tokenAddr, address[] dests, uint256[] values)
+  onlyOwner public
+  returns (uint256) {
+      uint256 i = 0;
+      while (i < dests.length) {
+         ERC20(_tokenAddr).transfer(dests[i], values[i]);
+         i += 1;
+      }
+      return(i);
+  }
+
+}
